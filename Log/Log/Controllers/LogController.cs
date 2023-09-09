4298684023b952f75552199188ac84c764323424
Reza <Reza@DESKTOP-CKL538G>
2023-09-09 20:19:16 +0330

    Initial .NET log and gateway

---
+++ Log/Log/Controllers/LogController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Log.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+        // GET: api/<LogController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { $"Your log created at {DateTime.Now}", "Log service!" };
+        }
+
+        // GET api/<LogController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LogController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LogController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LogController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Log/Log/Program.cs
+++ Log/Log/Program.cs
@@ -1,10 +1,4 @@
-using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 
@@ -12,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -26,7 +19,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseOcelot().Wait();
+
 app.MapControllers();
 
 app.Run();
