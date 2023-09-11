bf810383b8516809e9c896efa31d80d03d140dfc
Reza <Reza@DESKTOP-CKL538G>
2023-09-11 15:24:23 +0330

    Change Gateway.csproj

--- Gateway/Gateway/Program.cs
+++ Gateway/Gateway/Program.cs
@@ -6,27 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 app.UseOcelot().Wait();
-app.MapControllers();
 
 app.Run();
